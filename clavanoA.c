3bf9e16de6243cad7d8261d1fc8db2c7f827e19d
clavzno <76804492+clavzno@users.noreply.github.com>
2023-03-11 13:03:37 +0800

    clavanoA import

---
+++ clavanoA.c
@@ -0,0 +1,183 @@
+/*************
+CCPROG2 S18 GROUP <NUMBER>
+Name: Clavano, Angelica Therese I.
+Date created: 2023-02-22
+Last modified: 2023-02-23
+
+Notes: (1) won't run in Repl.it console, (2) vsCode Terminal won't show flashing colors, (3) Win11 Terminal shows no difference between bold and non-bold colors. 
+(4) exe file crashes when manually selected. (5) runs when F11 in devc++
+*************/
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h> //Required for the display_loading function, alternate is <windows.h>
+
+/* Define directives for cosmetic purposes in the program such as the 
+main menu and headers when under a certain module. */ 
+#define ANSI_FLASH_RED "\e[0;5;31m"
+#define ANSI_FLASH_BLUE "\e[0;5;34m"
+#define ANSI_FLASH_WHITE "\e[0;5;1m"
+#define ANSI_RED "\e[0;31m"
+#define ANSI_BLUE "\e[0;34m"
+#define ANSI_WHITE "\e[0;1m"
+#define ANSI_OFF "\e[m"
+// COLOR_OFF to end command; 1 = bold, 5 = slow blink, Xm = color
+
+/* Extra: Loading Screen Simulation */
+/* requires <unistd.h>/<windows.h> for sleep */
+void
+display_loading()
+{
+	int i, j, a;
+
+	printf(ANSI_WHITE "Loading" ANSI_OFF);
+
+	for (i = 0; i <= 6; i++)
+	{
+		for (j = 0; sleep(1); j++) //sleep delays the printing of "." for 1sec
+			j = a;
+		printf(ANSI_WHITE "." ANSI_OFF);
+	}
+	printf("\n");
+
+    system("cls"); //clears the screen
+}
+
+
+
+
+/* Called when "Vaccination Registration" option is chosen by user */
+void
+menu_vaxregistration()
+{
+	printf("You are in Vaccination Registrsation and Data Management.\n");
+}
+/* Below are subfunctions/menus to be called to preserve code readability. The
+programmer is aware of the amount of pointer variables to be passed within these
+subfunctions. */
+void
+vaxreg_useregistration() {
+	printf("You are in User Registration.\n");
+}
+void
+vaxreg_vaxappointment() {
+	printf("You are in Vaccination Appointment.\n");
+}
+void
+vaxreg_faqs() {
+	printf("You are in FAQs.\n");
+}
+void
+vaxreg_exit() {
+	system("cls");
+	menu_main();
+}
+
+
+
+/* During Vaccination Registration and Data Management, the values in selected
+user and appointment files are saved, preserved, and can be edited. Every chatbot
+interaction including questions and answers are saved and preserved. */
+void
+generate_userfile() {
+	printf("Added to user file.\n");
+}
+void
+generate_appointmentfile() {
+	printf("Added to appointment file.\n");
+}
+void
+generate_chatfile() {
+	printf("Conversation has been added to chat file.");
+}
+
+
+
+
+/* Called when "Manage Data" option is chosen by the user */
+void
+menu_datamanagement()
+{
+	printf("You are in Data Management.\n");
+}
+/* Below are subfunctions/menus to be called to preserve code readability. The
+programmer is aware of the amount of pointer variables to be passed within these
+subfunctions. */
+void
+data_user() {}
+void
+data_appt() {}
+void
+data_chatbot() {}
+void
+data_export() {}
+void
+data_import() {}
+void
+data_exit() {}
+
+
+
+
+/* Extra: Called when "Accessibility" option is chosen by the user */
+void 
+menu_accessibilty() {} //allows for options to remove
+/* Below are subfunctions/menus to be called to preserve code readability. The
+programmer is aware of the amount of pointer variables to be passed within these
+subfunctions. */
+void acc_removeblinking() {} //allows user to remove flashing colors
+void acc_language() {} //allows user to choose between english and filipino
+void acc_bgcolor() {} //allows user to choose between darkmode and lightmode for the terminal
+
+/* Displays main menu offering 2 options: Vaccination Registration Menu
+("Vaccination Registration") and Data Management Menu ("Manage Data") */
+int
+menu_main()
+{
+	int choice;
+	printf("**************************\n");
+	printf("\n");
+	printf(ANSI_WHITE "Welcome to" ANSI_OFF ANSI_FLASH_RED " Vax" ANSI_OFF ANSI_FLASH_BLUE "Bot" ANSI_OFF ANSI_FLASH_WHITE "PH!" ANSI_OFF "\n");
+	printf("\n");
+	printf("Select Menu:\n");
+    printf("[1] Vaccination Registration\n");
+    printf("[2] Manage Data\n");
+    printf("[3] Accessibility\n");
+    printf("[4] Exit\n");
+    printf("\n");
+    printf("**************************\n");
+    printf("\n");
+    printf("Enter choice: ");
+	scanf("%d", &choice);
+	printf("\n");
+
+	switch (choice)
+	{
+		case 1:
+			menu_vaxregistration();
+			break;
+		case 2:
+			menu_datamanagement();
+			break;
+		case 3:
+			menu_accessibilty();
+			break;
+		case 4:
+			exit(0);
+			break;
+		default:
+			printf("Invalid input. Please try again.\n");
+			menu_main();
+	}
+}
+
+
+/***********************************************************************/
+/* Driver code */
+int
+main()
+{
+	display_loading();
+	menu_main();
+	return 0;
+}
