733df98506d0861d07e3c2cfe0dab8ada03169a3
clavzno <76804492+clavzno@users.noreply.github.com>
2023-03-22 15:41:44 +0800

    cdg.c upload

---
+++ cdg.c
@@ -0,0 +1,20 @@
+/***************
+CCPROG2 S18 GRP <NUMBER>
+Clavano, Angelica Therese
+De Grano, Justin Patrick
+
+Date Created: 2023-03-22
+Date Modified:
+
+***************/
+
+/* Library declarations */
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h> //Required for the display_loading function, alternate is <windows.h>
+
+
+/* Define directives for max users since dynamic memory allocation is hard */
+#define MAX_USERS 100
+
+
